b6e68389166b08e30464b7e86c0eb5ce014c0e2a
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-24 14:37:37 -0300

    Home

--- app/parts/estrutura/footer.tsx
+++ app/parts/estrutura/footer.tsx
@@ -9,9 +9,9 @@ const Footer = () => {
         <footer className="bg-red-900 py-8 md:py-12 block p-4">
             <div className="container mx-auto px-4">
                 <div className="flex items-center flex-col">
-                    <h3 className="text-center text-2xl md:text-3xl mb-4 font-playfair font-bold text-yellow-500">Siga-nos no instagram</h3>
+                    <h3 className="text-center text-2xl md:text-3xl mb-4 font-playfair font-bold text-white">Siga-nos no instagram</h3>
                     <Link href={ContentLinks.instagram}>
-                        <button type="button" className="w-full border border-white text-white transition font-medium rounded-full my-2 py-3 px-8 flex justify-center items-center text-sm lg:text-base gap-3 transform hover:scale-105 duration-300">
+                        <button type="button" className="w-full hover:bg-yellow-500 hover:text-red-500 border border-yellow-500 text-white transition font-medium rounded-full my-2 py-3 px-8 flex justify-center items-center text-sm lg:text-base gap-3 transform hover:scale-105 duration-300">
                             <FontAwesomeIcon icon={faInstagram} className="h-4 md:h-6 w-4 md:w-6" /> 
                             <div> @umadqoficial</div>
                         </button>
--- app/parts/home/banner.tsx
+++ app/parts/home/banner.tsx
@@ -16,7 +16,7 @@ export default function Banner() {
                             <div className=" container mx-auto max-w-lg">
                                 <h2 className="text-4xl text-yellow-500 pb-4">11º CONGRESSO JOVEM</h2>
                                 <h3 className="text-xl pb-6 font-light">União de Mocidade das Assembleias de Deus em Queimados apresenta o tema de guerra espiritual baseado em efésios 6.</h3>
-                                <button className="border-2 border-whiite font-normal text-lg py-2 px-3 lg:px-6 rounded-md text-white flex items-center transition-all mx-auto md:mx-0"><span className="inline"> CONFIRMAR PRESENÇA </span></button>
+                                <button className="border-2 border-whiite hover:bg-white hover:text-red-500 font-normal text-lg py-2 px-3 lg:px-6 rounded-md text-white flex items-center transition-all mx-auto md:mx-0 hover:scale-105"><span className="inline"> CONFIRMAR PRESENÇA </span></button>
                             </div>
                         </div>
                         <div className="absolute z-10 md:relative -right-[65%] sm:-right-[55%] md:right-auto bottom-0 md:bottom-auto md:w-1/2 md:bg-guerreiro bg-contain bg-bottom bg-no-repeat h-full opacity-40 sm:opacity-40 md:opacity-40">
