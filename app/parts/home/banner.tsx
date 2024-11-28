c6c1c9a2e428b0345a17b1b6387b8ea2ae67ec84
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-28 17:42:25 -0300

    ajustado

--- app/parts/home/banner.tsx
+++ app/parts/home/banner.tsx
@@ -12,7 +12,10 @@ export default function Banner() {
                             <div className=" container mx-auto max-w-lg">
                                 <h2 className="text-4xl text-yellow-500 pb-4">11º CONGRESSO JOVEM</h2>
                                 <h3 className="text-xl pb-6 font-light">União de Mocidade das Assembleias de Deus em Queimados apresenta o tema de guerra espiritual baseado em efésios 6.</h3>
-                                <Link href="/midia"><button className="border-2 border-whiite hover:bg-white hover:text-red-500 font-normal text-lg py-2 px-3 lg:px-6 rounded-md text-white flex items-center transition-all mx-auto md:mx-0 hover:scale-105"><span className="inline"> ASSISTIR EVENTO </span></button></Link>
+                                <div className="flex gap-x-4">
+                                    <Link href="/programacao"><button className="border-2 border-whiite hover:bg-white hover:text-red-500 font-normal text-lg py-2 px-3 lg:px-6 rounded-md text-white uppercase flex items-center transition-all mx-auto md:mx-0 hover:scale-105"><span className="inline"> ASSISTIR EVENTO </span></button></Link>
+                                <Link href="/programacao"><button className="border-2 border-whiite hover:bg-white hover:text-red-500 font-normal text-lg py-2 px-3 lg:px-6 rounded-md text-white uppercase flex items-center transition-all mx-auto md:mx-0 hover:scale-105"><span className="inline"> Nosso Eventos </span></button></Link>
+                                </div>
                             </div>
                         </div>
                         <div className="absolute z-10 md:relative -right-[65%] sm:-right-[55%] md:right-auto bottom-0 md:bottom-auto md:w-1/2 md:bg-guerreiro bg-contain bg-bottom bg-no-repeat h-full opacity-40 sm:opacity-40 md:opacity-40">
