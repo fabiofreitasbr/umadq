c8cf2772177ac46503b8badf343467455f31319e
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-28 17:37:02 -0300

    Atualizado

--- app/parts/components/equipeList.tsx
+++ app/parts/components/equipeList.tsx
@@ -8,7 +8,7 @@ export default async function EquipeList(props: any) {
             {
                 data.map((dadosEquipe: any) => (
                     <div key={dadosEquipe.slug}>
-                        <h1 className="text-2xl uppercase my-4" id-key={dadosEquipe.id}>{dadosEquipe.name}</h1>
+                        <h1 className="text-lg md:text-xl xl:text-2xl uppercase my-4" id-key={dadosEquipe.id}>{dadosEquipe.name}</h1>
                         <EquipeVoluntarios id={dadosEquipe.id} />
                         <hr className="border-zinc-800 mt-4" />
                     </div>
--- app/parts/components/equipeVoluntariosSingle.tsx
+++ app/parts/components/equipeVoluntariosSingle.tsx
@@ -5,17 +5,17 @@ import Image from "next/image";
 
 export default function EquipeVoluntariosSingle(props: any) {
     return (
-        <div className="bg-zinc-900 rounded-2xl px-4 pt-8 pb-6" id-key={props.dados.id}>
+        <div className="bg-zinc-900 rounded-2xl px-2 md:px-4 pt-6 md:pt-8 pb-4 md:pb-6" id-key={props.dados.id}>
             <div className="text-center">
-                <Image src={`/img/equipe/${props.dados.instagram}.jpg`} width={112} height={112} alt="" className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28 mb-6" />
+                <Image src={`/img/equipe/${props.dados.instagram}.jpg`} width={112} height={112} alt="" className="w-16 h-16 m-auto rounded-full object-cover lg:w-28 lg:h-28 mb-6" />
                 <div className="mb-4">
-                    <h5 className="hidden mt-4 text-lg font-semibold text-white lg:block">{props.dados.name}</h5>
-                    <span className="hidden text-sm text-white lg:block">{props.dados.role}</span>
+                    <h5 className="mt-4 text-sm md:text-base xl:text-lg font-semibold text-white">{props.dados.name}</h5>
+                    <span className="text-xs md:text-sm text-white">{props.dados.role}</span>
                 </div>
                 <a href={`https://www.instagram.com/${props.dados.instagram}`} target="_blank">
-                    <button className="w-full bg-red-500 hover:bg-red-600 py-2 px-3 lg:px-6 rounded-md text-white flex items-center justify-center gap-x-2 transition-all text-sm">
+                    <button className="w-full bg-red-500 hover:bg-red-600 py-2 px-3 lg:px-6 rounded-md text-white flex items-center justify-center gap-x-2 transition-all text-xs md:text-sm">
                         <FontAwesomeIcon icon={faInstagram} className="w-4 h-4" />
-                        <span className="hidden lg:inline"> {excerptCharacters(props.dados.instagram, 15)} </span>
+                        <span className="inline"> {excerptCharacters(props.dados.instagram, 15)} </span>
                     </button>
                 </a>
             </div>
