9c1ff86856560e6fc6078559c2c2447e0b7f830f
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-28 18:13:09 -0300

    ajustado

--- app/parts/estrutura/footer.tsx
+++ app/parts/estrutura/footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     <Link href={ContentLinks.instagram}>
                         <button type="button" className="w-full hover:bg-yellow-500 hover:text-red-500 border border-yellow-500 text-white transition font-medium rounded-full my-2 py-3 px-8 flex justify-center items-center text-sm lg:text-base gap-3 transform hover:scale-105 duration-300">
                             <FontAwesomeIcon icon={faInstagram} className="h-4 md:h-6 w-4 md:w-6" /> 
-                            <div> @umadqoficial</div>
+                            <div>@umadqoficial</div>
                         </button>
                     </Link>
                 </div>
