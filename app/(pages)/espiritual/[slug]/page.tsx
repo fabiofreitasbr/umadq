6af77aa11b8755877b240895cc9c56e6716033aa
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-29 16:46:52 -0300

    Atualizado

--- app/(pages)/espiritual/[slug]/page.tsx
+++ app/(pages)/espiritual/[slug]/page.tsx
@@ -1,13 +1,19 @@
 import EspiritualSlug, { slugArticle } from '@/app/parts/components/espiritualSlug';
 import { Suspense } from 'react';
 import Loading from '@/app/parts/components/loading';
+import HeaderTitle from '@/app/parts/estrutura/headerTitle';
 
 export default function Page({ params }: slugArticle) {
     return (
         <>
-            <Suspense fallback={<Loading />}>
-                <EspiritualSlug params={params} />
-            </Suspense>
+            <HeaderTitle title="Espiritual" />
+            <section className="min-h-96 my-8">
+                <div className="flex flex-wrap justify-center">
+                    <Suspense fallback={<Loading />}>
+                        <EspiritualSlug params={params} />
+                    </Suspense>
+                </div>
+            </section>
         </>
     );
 }
