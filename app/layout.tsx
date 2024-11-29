c0c72959a6e24baf7fba1e7f18c7fb789edbdfae
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-29 00:38:17 -0300

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
 
@@ -16,7 +17,19 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={poppins.className + ` w-full overflow-x-hidden`}>{children}</body>
+            <body className={poppins.className + ` w-full overflow-x-hidden`}>
+
+                <Script id="google-analytics">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-3617HRGK6Y');
+                `}
+                </Script>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3617HRGK6Y" />
+                {children}
+                </body>
         </html>
     );
 }
