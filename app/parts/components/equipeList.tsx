3d6b4e4aff6ebae061b2f8270a23f9b763bf80db
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-29 17:09:07 -0300

    Ajuste Equipe

--- app/parts/components/equipeList.tsx
+++ app/parts/components/equipeList.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import EquipeVoluntarios from "./equipeVoluntarios";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default async function EquipeList(props: any) {
     const { data } = await axios.get(process.env.local_api + "equipe/find");
@@ -9,7 +11,9 @@ export default async function EquipeList(props: any) {
                 data.map((dadosEquipe: any) => (
                     <div key={dadosEquipe.slug}>
                         <h1 className="text-lg md:text-xl xl:text-2xl uppercase my-4" id-key={dadosEquipe.id}>{dadosEquipe.name}</h1>
-                        <EquipeVoluntarios id={dadosEquipe.id} />
+                        <Suspense fallback={<Loading />}>
+                            <EquipeVoluntarios id={dadosEquipe.id} />
+                        </Suspense>
                         <hr className="border-zinc-800 mt-4" />
                     </div>
                 ))
