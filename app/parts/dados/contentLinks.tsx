0c6ace1becf089baf9068776b33a88e1ad1e3976
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-29 16:42:20 -0300

    Atualizado

--- app/parts/dados/contentLinks.tsx
+++ app/parts/dados/contentLinks.tsx
@@ -1,22 +1,8 @@
 const ContentLinks = {
     
-	email: 'mailto:contato@meusite.com.br',
-    emailShow: 'contato@meusite.com.br',
-
-	phone: 'tel:+5521967013300',
-    phoneShow: '(21) 96701-3300',
-
-	whatsapp: 'https://wa.me/5521967013300',
-    whatsappShow: '(21) 96701-3300',
-    
-	facebook: 'https://facebook.com/',
+	facebook: 'https://www.facebook.com/umadqoficialbr',
 	instagram: 'https://www.instagram.com/umadqoficial',
-	instagramDaiane: 'https://instagram.com/umadqoficial',
-	youtube: '',
-    
-	maps: '',
-    address1: '',
-    address2: '',
+	youtube: 'https://www.youtube.com/@umadqoficial',
 };
 
 export default ContentLinks;
