218aa3b46ae08ddd752eba4195f2c38e11682751
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-12-01 00:31:36 -0300

    Atualizado 3.0.16

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,32 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-    	extend: {
-    		colors: {
-    			red: {
-    				'500': '#8d1d18',
-    				'600': '#680c0c',
-    				'900': '#1d0908'
-    			},
-    			yellow: {
-    				'500': '#eb9500'
-    			},
-    			bege: {
-    				'500': '#fddeb2'
-    			},
-    		},
-    		backgroundImage: {
-    			'fundo': 'url("/img/background.png")',
-    			'guerreiro': 'url("/img/guerreiro.png")',
-    		},
-    	}
+        extend: {
+            colors: {
+                red: {
+                    '500': '#8d1d18',
+                    '600': '#680c0c',
+                    '900': '#1d0908'
+                },
+                yellow: {
+                    '500': '#eb9500'
+                },
+                bege: {
+                    '500': '#fddeb2'
+                },
+            },
+            backgroundImage: {
+                'fundo': 'url("/img/background.png")',
+                'guerreiro': 'url("/img/guerreiro.png")',
+            },
+        }
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [],
 };
 export default config;
