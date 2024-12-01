bf6a123fe4bed705a3cfec1d2a438e515874de0b
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-11-30 23:41:01 -0300

    Atualizado 3.0.5

--- app/parts/loadings/loadingEquipe.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
-export default function LoadingEquipe() {
-    return (
-        <>
-            <Skeleton className="w-[100px] h-[20px] rounded-full" />
-        </>
-    )
-}
